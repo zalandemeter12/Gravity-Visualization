fcc5afa9f53390b2ef3db8d19f41bf209010255c
Demeter Zalán <zalandemeter12@gmail.com>
2021-05-05 16:54:09 +0200

    v3.2

--- Programs/Skeleton/Skeleton/Skeleton.cpp
+++ Programs/Skeleton/Skeleton/Skeleton.cpp
@@ -156,23 +156,22 @@ class PhongShader : public Shader {
 			vec4 wLightPos;
 		};
 
-		uniform mat4  MVP, M, Minv; // MVP, Model, Model-inverse
-		uniform Light[8] lights;    // light sources 
+		uniform mat4  MVP, M, Minv;
+		uniform Light[8] lights;
 		uniform int   nLights;
-		uniform vec3  wEye;         // pos of eye
+		uniform vec3  wEye;
 
-		layout(location = 0) in vec3  vtxPos;            // pos in modeling space
-		layout(location = 1) in vec3  vtxNorm;      	 // normal in modeling space
+		layout(location = 0) in vec3  vtxPos;
+		layout(location = 1) in vec3  vtxNorm;
 		layout(location = 2) in vec2  vtxUV;
 
-		out vec3 wNormal;		    // normal in world space
-		out vec3 wView;             // view in world space
-		out vec3 wLight[8];		    // light dir in world space
+		out vec3 wNormal;
+		out vec3 wView;
+		out vec3 wLight[8];
 		out vec2 texcoord;
 
 		void main() {
-			gl_Position = vec4(vtxPos, 1) * MVP; // to NDC
-			// vectors for radiance computation
+			gl_Position = vec4(vtxPos, 1) * MVP;
 			vec4 wPos = vec4(vtxPos, 1) * M;
 			for(int i = 0; i < nLights; i++) {
 				wLight[i] = lights[i].wLightPos.xyz * wPos.w - wPos.xyz * lights[i].wLightPos.w;
@@ -198,21 +197,21 @@ class PhongShader : public Shader {
 		};
 
 		uniform Material material;
-		uniform Light[8] lights;    // light sources 
+		uniform Light[8] lights;
 		uniform int   nLights;
 		uniform sampler2D diffuseTexture;
 
-		in  vec3 wNormal;       // interpolated world sp normal
-		in  vec3 wView;         // interpolated world sp view
-		in  vec3 wLight[8];     // interpolated world sp illum dir
+		in  vec3 wNormal;
+		in  vec3 wView; 
+		in  vec3 wLight[8];
 		in  vec2 texcoord;
 		
-        out vec4 fragmentColor; // output goes to frame buffer
+        out vec4 fragmentColor;
 
 		void main() {
 			vec3 N = normalize(wNormal);
 			vec3 V = normalize(wView); 
-			if (dot(N, V) < 0) N = -N;	// prepare for one-sided surfaces like Mobius or Klein
+			if (dot(N, V) < 0) N = -N;
 			vec3 texColor = texture(diffuseTexture, texcoord).rgb;
 			vec3 ka = material.ka * texColor;
 			vec3 kd = material.kd * texColor;
@@ -222,7 +221,6 @@ class PhongShader : public Shader {
 				vec3 L = normalize(wLight[i]);
 				vec3 H = normalize(L + V);
 				float cost = max(dot(N,L), 0), cosd = max(dot(N,H), 0);
-				// kd and ka are modulated by the texture
 				radiance += ka * lights[i].La + 
                            (kd * texColor * cost + material.ks * pow(cosd, material.shininess)) * lights[i].Le / pow(length(lights[i].wLightPos),2);
 			}
@@ -258,25 +256,24 @@ class PhongShaderSheet : public Shader {
 			vec4 wLightPos;
 		};
 
-		uniform mat4  MVP, M, Minv; // MVP, Model, Model-inverse
-		uniform Light[8] lights;    // light sources 
+		uniform mat4  MVP, M, Minv;
+		uniform Light[8] lights; 
 		uniform int   nLights;
-		uniform vec3  wEye;         // pos of eye
+		uniform vec3  wEye;       
 
-		layout(location = 0) in vec3  vtxPos;            // pos in modeling space
-		layout(location = 1) in vec3  vtxNorm;      	 // normal in modeling space
+		layout(location = 0) in vec3  vtxPos;       
+		layout(location = 1) in vec3  vtxNorm;     
 		layout(location = 2) in vec2  vtxUV;
 
-		out vec3 wNormal;		    // normal in world space
-		out vec3 wView;             // view in world space
-		out vec3 wLight[8];		    // light dir in world space
+		out vec3 wNormal;		 
+		out vec3 wView;          
+		out vec3 wLight[8];		 
 		out vec2 texcoord;
 		out float z;
 
 		void main() {
-			gl_Position = vec4(vtxPos, 1) * MVP; // to NDC
+			gl_Position = vec4(vtxPos, 1) * MVP;
 			z = vtxPos.z;
-			// vectors for radiance computation
 			vec4 wPos = vec4(vtxPos, 1) * M;
 			for(int i = 0; i < nLights; i++) {
 				wLight[i] = lights[i].wLightPos.xyz * wPos.w - wPos.xyz * lights[i].wLightPos.w;
@@ -302,22 +299,22 @@ class PhongShaderSheet : public Shader {
 		};
 
 		uniform Material material;
-		uniform Light[8] lights;    // light sources 
+		uniform Light[8] lights; 
 		uniform int   nLights;
 		uniform sampler2D diffuseTexture;
 
-		in  vec3 wNormal;       // interpolated world sp normal
-		in  vec3 wView;         // interpolated world sp view
-		in  vec3 wLight[8];     // interpolated world sp illum dir
+		in  vec3 wNormal;  
+		in  vec3 wView;         
+		in  vec3 wLight[8];    
 		in  vec2 texcoord;
 		in float z;
 		
-        out vec4 fragmentColor; // output goes to frame buffer
+        out vec4 fragmentColor; 
 
 		void main() {
 			vec3 N = normalize(wNormal);
 			vec3 V = normalize(wView); 
-			if (dot(N, V) < 0) N = -N;	// prepare for one-sided surfaces like Mobius or Klein
+			if (dot(N, V) < 0) N = -N;	
 			vec3 texColor = texture(diffuseTexture, texcoord).rgb;
 			vec3 ka = material.ka * texColor;
 			vec3 kd = material.kd * texColor;
@@ -349,7 +346,6 @@ class PhongShaderSheet : public Shader {
 				vec3 L = normalize(wLight[i]);
 				vec3 H = normalize(L + V);
 				float cost = max(dot(N,L), 0), cosd = max(dot(N,H), 0);
-				// kd and ka are modulated by the texture
 				radiance += ka * lights[i].La + 
                            (kd * texColor * cost + material.ks * pow(cosd, material.shininess)) * lights[i].Le / pow(length(lights[i].wLightPos),2);
 			}
